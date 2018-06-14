e9380fd3530e4c00f0c3973956734f31a2d1d547
Henrik Korsgaard <korsgaard@cs.au.dk>
2018-06-14 10:48:31 +0000

    fixed update on reboot

--- update.sh
+++ update.sh
@@ -7,11 +7,10 @@ if nc -zw1 $test 443 && echo |openssl s_client -connect $test:443 2>&1 |awk '
   handshake && $1 == "Verification" { if ($2=="OK") exit; exit 1 }
   $1 $2 == "SSLhandshake" { handshake = 1 }'
 then
-  echo "we have connectivity"
+    # Updating GoProxi from Github
+    echo "Updating GoProxi from Github"
+    cd /opt/goproxi
+    git pull
 fi
 
-# Updating GoProxi from Github
-echo "Updating GoProxi from Github"
-cd /opt/goproxi
-git pull
 

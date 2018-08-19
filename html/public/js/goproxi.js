d945306966fe9491d45a8a8c3c37bb815925f0ab
Henrik Korsgaard <korsgaard@cs.au.dk>
2018-08-19 20:43:16 +0000

    updated html files

--- html/public/js/goproxi.js
+++ html/public/js/goproxi.js
@@ -1,3 +1,4 @@
+
 (function (global) {
 
     var socket, reconnect, attempts = 0,
@@ -14,27 +15,41 @@
             clearInterval(reconnect)
             attempts = 0;
 
-            fetchThis().then(function (data) {
+            fetchLocations().then(function(locationData){
+               
+                placeObserver.locations = locationData.response;
 
+                fetchDevices().then(function(deviceData){
+                    var evt = {
+                        type: "ready"
+                    };
+                    placeObserver.devices = deviceData.response;
+                    emmitPlaceEvent(evt);
+                });
+            });
+
+            fetchClient().then(function (data) {
+                
                 var evt = {
                     type: "ready"
-                }
+                };
                 
-                deviceObserver.proximity = data.response.proximity
-                deviceObserver.locations = data.response.locations
-                delete data.response.locations
-                delete data.response.proximity
-                deviceObserver.device = data.response
+                deviceObserver.proximity = data.response.proximity;
+                deviceObserver.locations = data.response.locations;
+                delete data.response.locations;
+                delete data.response.proximity;
+                deviceObserver.device = data.response;
 
                 emmitDeviceEvent(evt)
                 fetchLocation(deviceObserver.proximity.mac).then(function(locationData){
-                    locationObserver.devices = locationData.response.devices
-                    delete data.response.devices
-                    locationObserver.location = locationData.response
-                    subscribeLocation(locationData.response.mac)
-                    emmitLocationEvent(evt)
+                    
+                    locationObserver.devices = locationData.response.devices;
+                    delete data.response.devices;
+                    locationObserver.location = locationData.response;
+                    subscribeLocation(locationData.response.mac);
+                    emmitLocationEvent(evt);
                 }).catch(function(err){
-                    console.log(err)
+                    console.log(err);
                 })
             }).catch(function (err) {
                 console.log(err);
@@ -42,17 +57,17 @@
         }
 
         socket.onclose = function (e) {
-            console.log("Goproxi: Websocket connection closed. Attempting to reconnect!")
+            console.log("Goproxi: Websocket connection closed. Attempting to reconnect!");
             for (var k in communications) {
-                communications[k]("Websocket connection closed!")
+                communications[k]("Websocket connection closed!");
             }
             reconnect = setInterval(function () {
                 attempts++;
-            }, timeout * attempts)
+            }, timeout * attempts);
         }
 
         socket.onmessage = function (e) {
-            data = JSON.parse(e.data)
+            data = JSON.parse(e.data);
 
             if (data.hasOwnProperty("event")) {
                 
@@ -64,29 +79,29 @@
                 
                 if (data.type === "DeviceEvent") {
                     if (data.event === "DeviceChangedProximityZone") {
-                        emmitDeviceEvent(evt)
+                        emmitDeviceEvent(evt);
                     } else if (data.event === "DeviceChangedProximity") {
                         updateLocation().then(function(){
-                            emmitDeviceEvent(evt)
+                            emmitDeviceEvent(evt);
                         }).catch(function(err){
-                            console.log(err)
+                            console.log(err);
                         })
                     }
                 } else if (data.type === "LocationEvent") {
                     if (data.event === "DeviceChangedProximityZone") {
-                        emmitLocationEvent(evt)
+                        emmitLocationEvent(evt);
                     } else if (data.event === "DeviceJoinedLocation") {
-                        emmitLocationEvent(evt)
+                        emmitLocationEvent(evt);
                     } else if (data.event === "DeviceLeftLocation") {
-                        emmitLocationEvent(evt)
+                        emmitLocationEvent(evt);
                     }
                 }
                 //we can filter for location events vs device events
             } else if (data.id && communications.hasOwnProperty(data.id)) {
-                var fn = communications[data.id]
-                delete communications[data.id]
+                var fn = communications[data.id];
+                delete communications[data.id];
 
-                fn(null, data)
+                fn(null, data);
             }
         }
 
@@ -96,7 +111,7 @@
         if (deviceListeners.hasOwnProperty(event.type)) {
             var listeners = deviceListeners[event.type]
             for (var i = 0, n = listeners.length; i < n; i++) {
-                listeners[i](event)
+                listeners[i](event);
             }
         } else {
             console.log("Error: Unknown event type " + event.type)
@@ -107,18 +122,30 @@
         if (locationListeners.hasOwnProperty(event.type)) {
             var listeners = locationListeners[event.type]
             for (var i = 0, n = listeners.length; i < n; i++) {
-                listeners[i](event)
+                listeners[i](event);
             }
         } else {
-            console.log("Error: Unknown event type " + event.type)
+            console.log("Error: Unknown event type " + event.type);
+        }
+
+    }
+
+    function emmitPlaceEvent(event) {
+        if (placeListeners.hasOwnProperty(event.type)) {
+            var listeners = placeListeners[event.type]
+            for (var i = 0, n = listeners.length; i < n; i++) {
+                listeners[i](event);
+            }
+        } else {
+            console.log("Error: Unknown event type " + event.type);
         }
 
     }
 
     function sendMessage(msg, callback) {
         msg.id = guid() + Date.now();
-        communications[msg.id] = callback
-        socket.send(JSON.stringify(msg))
+        communications[msg.id] = callback;
+        socket.send(JSON.stringify(msg));
     }
 
     function guid() {
@@ -132,35 +159,35 @@
 
     function updateLocation(){
         return new Promise(function (rs, rj) {
-            unsubscribeLocation(deviceObserver.proximity.mac)
+            unsubscribeLocation(deviceObserver.proximity.mac);
             fetchThis().then(function(data){
                 
-                deviceObserver.proximity = data.response.proximity
-                deviceObserver.locations = data.response.locations
-                delete data.response.locations
-                delete data.response.proximity
-                deviceObserver.device = data.response
+                deviceObserver.proximity = data.response.proximity;
+                deviceObserver.locations = data.response.locations;
+                delete data.response.locations;
+                delete data.response.proximity;
+                deviceObserver.device = data.response;
 
                 fetchLocation(deviceObserver.proximity.mac).then(function(locationData){
-                    locationObserver.devices = locationData.response.devices
-                    delete data.response.devices
-                    locationObserver.location = locationData.response
-                    subscribeLocation(locationData.response.mac)
-                    rs()
-                }).catch(rj)
-            }).catch(rj)
+                    locationObserver.devices = locationData.response.devices;
+                    delete data.response.devices;
+                    locationObserver.location = locationData.response;
+                    subscribeLocation(locationData.response.mac);
+                    rs();
+                }).catch(rj);
+            }).catch(rj);
         })
     }
 
-    function fetchThis() {
+    function fetchClient() {
         return new Promise(function (rs, rj) {
             sendMessage({
                 "request": "this"
             }, function (err, data) {
                 if (err) {
-                    rj(err)
-                };
-                rs(data)
+                    rj(err);
+                }
+                rs(data);
             })
         });
     }
@@ -172,25 +199,53 @@
                 "mac": mac
             }, function (err, data) {
                 if (err) {
-                    rj(err)
-                };
-                rs(data)
-            })
+                    rj(err);
+                }
+                rs(data);
+            });
         });
     }
+
+    function fetchLocations() {
+        return new Promise(function (rs, rj) {
+            sendMessage({
+                "request": "locations"                
+            }, function (err, data) {
+                if (err) {
+                    rj(err);
+                }
+                rs(data);
+            });
+        });
+    }
+
+    function fetchDevices() {
+        return new Promise(function (rs, rj) {
+            sendMessage({
+                "request": "devices"
+            }, function (err, data) {
+                if (err) {
+                    rj(err);
+                }
+                rs(data);
+            });
+        });
+    }
+
+
 
     function subscribeLocation(mac) {
         sendMessage({
             "request": "subscribe",
             "mac": mac
-        }, function (err, data) {})
+        }, function (err, data) {});
     }
 
     function unsubscribeLocation(mac) {
         sendMessage({
             "request": "unsubscribe",
             "mac": mac
-        }, function (err, data) {})
+        }, function (err, data) {});
     }
 
     var locationListeners = {
@@ -198,33 +253,41 @@
         "DeviceChangedProximityZone": [],
         "DeviceJoinedLocation": [],
         "DeviceLeftLocation": []
-    }
+    };
+
+    var placeListeners = {
+        "ready": [],
+        "DeviceJoined": [],
+        "DeviceLeft": [],
+        "LocationDiscovered": [],
+        "LocationDisappeared": []
+    };
 
     var deviceListeners = {
         "ready": [],
         "DeviceChangedProximityZone": [],
         "DeviceChangedProximity": []
-    }
+    };
 
     var deviceObserver = {
         on: function (event, handler) {
             var events = event.split(" ")
             for (var i = 0, n = events.length; i < n; i++) {
                 if (deviceListeners.hasOwnProperty(events[i]) && typeof (handler) === 'function') {
-                    deviceListeners[events[i]].push(handler)
+                    deviceListeners[events[i]].push(handler);
                 } else {
-                    handler("Error: Unknown event type " + event)
+                    handler("Error: Unknown event type " + event);
                 }
             }
         },
         locations: [],
         device: {},
         proximity: {},
-    }
+    };
 
     var locationObserver = {
         on: function (event, handler) {
-            var events = event.split(" ")
+            var events = event.split(" ");
             for (var i = 0, n = events.length; i < n; i++) {
                 if (locationListeners.hasOwnProperty(events[i]) && typeof (handler) === 'function') {
                     locationListeners[events[i]].push(handler)
@@ -235,12 +298,28 @@
         },
         devices: [],
         location: {}
-    }
+    };
+
+    var placeObserver = {
+        on: function (event, handler) {
+            var events = event.split(" ");
+            for (var i = 0, n = events.length; i < n; i++) {
+                if (placeListeners.hasOwnProperty(events[i]) && typeof (handler) === 'function') {
+                    placeListeners[events[i]].push(handler)
+                } else {
+                    handler("Error: Unknown event type " + event)
+                }
+            }
+        },
+        devices: [],
+        locations: []
+    };
 
     goproxi = {
         DeviceObserver: deviceObserver,
-        LocationObserver: locationObserver
-    }
+        LocationObserver: locationObserver,
+        PlaceObserver: placeObserver
+    };
 
-    this.goproxi = goproxi
-})(this)
+    this.goproxi = goproxi;
+})(this);

0298fb56b6dbc2cf9fd02ff336ac333f3455f45f
Henrik Korsgaard <40322+henrikkorsgaard@users.noreply.github.com>
2023-03-06 14:08:49 +0100

    made some progression on the packet sniffer

--- proximity/network/network.go
+++ proximity/network/network.go
@@ -8,34 +8,53 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
+
+
 func MonitorNetworkTraffic(iface string) {
+	/*
+		When we open live on a wifi device, we get a handle with only the radioTap linktype and 802.11 plus radiotap header. That give us access to the following packets:
+		radioLayer:https://pkg.go.dev/github.com/google/gopacket@v1.1.19/layers#RadioTap
+		dot11Layer: https://pkg.go.dev/github.com/google/gopacket@v1.1.19/layers#Dot11
+
+		any other linklayer will be empty
+		See on decryption: https://security.stackexchange.com/a/186116
+	*/
+
 	handle, err := pcap.OpenLive(iface, 96, true, pcap.BlockForever)
 	defer handle.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
 	var radioLayer layers.RadioTap
 	var dot11Layer layers.Dot11
-
+	
 	parser := gopacket.NewDecodingLayerParser(
 		layers.LayerTypeRadioTap,
 		&radioLayer,
 		&dot11Layer,
-		//We need more packet layers for the simulator, but lets see
 	)
 
 	foundLayerTypes := []gopacket.LayerType{}
-	for packet := range packetSource.Packets() {
 
-		parser.DecodeLayers(packet.Data(), &foundLayerTypes)
+	for {
+		packet,_,err := handle.ReadPacketData()
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		for packet := range packetSource.Packets() {
-			parser.DecodeLayers(packet.Data(), &foundLayerTypes)
-			fmt.Printf("%#v\n", radioLayer)
-			fmt.Printf("%#v\n", dot11Layer)		
+		parser.DecodeLayers(packet, &foundLayerTypes)
+		//see: https://networkengineering.stackexchange.com/questions/25100/four-layer-2-addresses-in-802-11-frame-header
+		//The ideal case for capturing signal strength between the node and device
+		//is when addr1 and addr3 is equal to the station mac and addr2 is not equal
+		//to station mac
+		if len(foundLayerTypes) >= 2 && radioLayer.DBMAntennaSignal != 0 {
+		
+			fmt.Println(radioLayer.DBMAntennaSignal)
+			fmt.Println(dot11Layer.Address1.String())
+			fmt.Println(dot11Layer.Address2.String())
+			fmt.Println(dot11Layer.Address3.String())
+			fmt.Println(dot11Layer.Address4.String())
 		}
 	}
 }
